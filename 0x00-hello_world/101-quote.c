d43d1dd86977bd9f244986369dd84b87bdf331a6
uwakwevivian <uwakwevivian@gmail.com>
2022-12-08 18:40:36 -0800

    simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	print("and that piece of at is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
