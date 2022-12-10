74032684f4fede1addf0d26e56d18a37706bc1b7
uwakwevivian <uwakwevivian@gmail.com>
2022-12-10 10:08:34 -0800

    last values

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
-		printf("The last digit of %d is %d is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d is greater than 5\n", n, n % 10);
 	else if (n % 10 == 0)
-		printf("The lst digit of %d is %d and is 0\n", n, n% 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
