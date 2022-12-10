0b63755cc98f35716791db1b032931dd0c37bacb
uwakwevivian <uwakwevivian@gmail.com>
2022-12-10 11:09:48 -0800

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0
+		while (x <= 9)
+		{
+			printf("%d", x);
+				x++;
+		}
+		printf("\n");
+		return (0);
+}
+
