b7d54e8b0c9929950420930deddcad90c55ba720
uwakwevivian <uwakwevivian@gmail.com>
2022-12-10 10:40:39 -0800

    upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+	char X = 'A'
+		while (x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+		while (X <= 'Z')
+		{
+			putchar(X);
+			X++;
+		}
+		putchar('\n')
+		return (0);
+}
+
