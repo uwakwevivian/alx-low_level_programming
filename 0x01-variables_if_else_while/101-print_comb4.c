066f7136809d1227581268537701b3f5e8a50c7c
uwakwevivian <uwakwevivian@gmail.com>
2022-12-11 05:06:13 -0800

    possible combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = '1'; y <= '8'; y++)
+		{
+			for (z='2'; z <= '9'; z++)
+			{
+				if (x < y && y < z)
+				{
+				putchar(x);
+				putchar(y);
+				putchar(x);
+				if (x == '7' && y == '8' && z == '9')
+				break;
+				putchar(',');
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
