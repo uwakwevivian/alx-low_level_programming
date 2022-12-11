250d0aa3e282639a2a7e3a3ac06c4f62aba6371c
uwakwevivian <uwakwevivian@gmail.com>
2022-12-11 03:12:19 -0800

    combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = '0';
+
+		while (x <= '9')
+		{
+			putchar(x);
+			if (x != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			x++;
+		}
+		putchar('\n');
+		return (0);
+}
+
