3a3cfc5123ef0385d02e702d426b4b25971c3b18
uwakwevivian <uwakwevivian@gmail.com>
2022-12-14 10:35:21 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Number to return it's Absolute value
+ * @Num: Int value to be checked
+ * Return: Last digit of a number
+ */
+int print_last_digit(int Num)
+{
+	int n = Num % 10;
+
+	if (n < 0)
+		n = -n;
+
+	_putchar(n + '0');
+	return (n);
+}
