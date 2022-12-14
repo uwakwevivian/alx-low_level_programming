bad8280fbe9405aae2877f36abab7d7685f6cb9e
uwakwevivian <uwakwevivian@gmail.com>
2022-12-14 06:56:51 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign -Checks the sign of a number
+ * @n: Int value to be checked
+ * Return: 1 if +ve, 0 if 0, -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
