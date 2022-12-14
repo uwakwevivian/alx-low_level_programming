9eecdf7e3611770d8e52f66cf9b54b7c25dda3bc
uwakwevivian <uwakwevivian@gmail.com>
2022-12-14 04:54:17 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+}
