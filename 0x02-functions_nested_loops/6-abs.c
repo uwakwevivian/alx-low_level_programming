693bec4f98d64a6baf6be8b567dd77d933178afa
uwakwevivian <uwakwevivian@gmail.com>
2022-12-14 07:44:52 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Number to return it's Absolute value
+ * @a: Int value to be checked
+ * Return: Absolute value
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = -x;
+
+		return (x);
+	}
+	else
+	{
+		return (x);
+	}
+}
