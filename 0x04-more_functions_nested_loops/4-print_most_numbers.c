b4a2429bda1e08dbcb2051861ffd697a62424d61
uwakwevivian <uwakwevivian@gmail.com>
2022-12-16 15:19:48 -0800

    loops 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers 0 to 9
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 0; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
