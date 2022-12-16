2df1f40f82797761cbbd44ec125bf132727049d4
uwakwevivian <uwakwevivian@gmail.com>
2022-12-16 13:17:41 -0800

    loops 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit -checks if numbers are 0 through 9
+ * @c: char to check
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+
+{
+	int multi = a * b;
+
+	return multi = a * b;
+			return (multi);
+}
