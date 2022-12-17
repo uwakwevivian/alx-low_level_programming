125480986d31397422e340b5a3467840d5c36019
uwakwevivian <uwakwevivian@gmail.com>
2022-12-16 18:51:40 -0800

    loops 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - code to print a FizzBuzz numbering
+ *
+ *
+ * Return: Always 0 ()
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	for (i = 100; i < 101; i++)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+	putchar('\n');
+	return (0);
+
+}
