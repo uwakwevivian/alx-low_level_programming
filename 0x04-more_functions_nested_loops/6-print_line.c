e019bf67249cc1ca6ecb33b2e0655d57d003ca27
uwakwevivian <uwakwevivian@gmail.com>
2022-12-16 16:31:45 -0800

    loops 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function to print a line
+ * @n: line length control value
+ * Return: returns a line based on value of n
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
