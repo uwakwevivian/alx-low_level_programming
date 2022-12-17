c704bc6ced8e96feeb4485e804f66526a0dd8951
uwakwevivian <uwakwevivian@gmail.com>
2022-12-16 17:00:55 -0800

    loop 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,32 +3,27 @@
 /**
  * print_diagonal - function to print a line
  * @n: line length control value
- * @i - hihi
  * Return: returns a diagonal line based on value of n
  */
 void print_diagonal(int n)
 {
-	int i = 1;
 
-	if (n > 0)
+	int a, b;
+
+	if (n <= 0)
 	{
-		_putchar('/');
-
-		while (i < n)
-		{
-			int count = 0;
-
-			_putchar('\n');
-			while (count < i)
-			{
-				_putchar(' ');
-				count++;
-			}
-			_putchar('/')
-			i++;
-		}
 		_putchar('\n');
 	}
 	else
-		_putchar('\n');
+	{
+		for  (a = 1; a <= n; a++)
+		{
+			for (b = 1; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			putchar('\n');
+		}
+	}
 }
