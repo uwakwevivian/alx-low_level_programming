de968671e171ae4ba0e0eefb84a721f04a14fa17
uwakwevivian <uwakwevivian@gmail.com>
2022-12-20 18:33:46 -0800

    Pointers 4 updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,24 @@
 #include "main.h"
 
-int main(void)
+/**
+ * print_rev - function to print reverse of a string
+ * @s: string in question
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
 {
-	char *str;
-	
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return(0);
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar('\n');
 }
