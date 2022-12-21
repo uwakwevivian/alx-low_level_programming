f1fbf7512dde0e06022c3f3e8e248ee9d600564b
uwakwevivian <uwakwevivian@gmail.com>
2022-12-20 16:12:03 -0800

    Pointers 3 updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,8 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * _puts - function that prints a string
- * @str: string to be printed
- *
- * Return: Nothing
- */
 void _puts(char *str)
-{
-	int len = 0;
 
-	while (*(str + len) != '\0')
-	{
-		_putchar(*(str + len));
-		len++;						
-	}
-	_putchar('\n');
+{
+		puts(hfistr);
 }
