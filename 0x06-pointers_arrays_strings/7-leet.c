a0c75b96fd378279aa9fe59c8166e2f50c1a67ab
uwakwevivian <uwakwevivian@gmail.com>
2022-12-22 07:57:30 -0800

    more pointers 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - function to encode some alphas to nums
+ * @str: pointer to the string
+ *
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char a[] = {'a', 'e', 'o', 't', 'l'};
+	char A[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {'4', '3', '0', '7', '1'};
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (j < 5)
+		{
+			if (str[i] == a[j] || str[i] == A[j])
+				str[i] = num[j];
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
