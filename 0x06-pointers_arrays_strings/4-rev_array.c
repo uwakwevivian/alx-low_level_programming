408926d7862e4727cae24ae317a8cc86c34157b2
uwakwevivian <uwakwevivian@gmail.com>
2022-12-22 04:39:26 -0800

    moreon arrays 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - function to reverse an array
+ * @a: array in question
+ * @n: no. of array elements#
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, storage;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		storage = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = storage;
+	}
+}
