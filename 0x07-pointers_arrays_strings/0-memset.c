2012379110d9d63ddc3cdcb81f9cf5d8bb094023
uwakwevivian <uwakwevivian@gmail.com>
2023-01-02 13:30:50 -0800

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @n: bytes to be filled
+ * @b: constant
+ * @s: point to string
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned char n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
