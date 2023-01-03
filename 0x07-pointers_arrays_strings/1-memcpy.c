62c52374333caf2c412f3a65c626cba08dc5bede
uwakwevivian <uwakwevivian@gmail.com>
2023-01-03 06:01:36 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * _memcpy -> memory copy
+ * @dest: is destination memory
+ * @src: is source memory
+ * @n: number of byte to be copied
+ *
+ * Return: String copied from source
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	dest[a] = src[a];
+	return (dest);
+
