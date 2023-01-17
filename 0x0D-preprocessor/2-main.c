8ca6856c5475cf283e7265180d64c9829a337a99
uwakwevivian <uwakwevivian@gmail.com>
2023-01-17 13:58:32 -0800

    all task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name where the file program 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define abs(x) ((x < 0) ? (x) * (-1) : (x))
+#endif
