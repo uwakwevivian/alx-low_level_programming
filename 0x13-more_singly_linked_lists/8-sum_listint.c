21c52622c5ff7383b593c12e338a976067133f6b
uwakwevivian <uwakwevivian@gmail.com>
2023-01-31 17:25:02 -0800

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,7 @@
-include "lists.h"
+#include "lists.h"
 
 /**
  * sum_listint - calculates the sum of all the data in a listint_t list
- *
  * @head: first node in the linked list
  *
  * Return: resulting sum
