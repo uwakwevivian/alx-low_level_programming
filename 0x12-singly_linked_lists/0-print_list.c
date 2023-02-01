a765145ff372fe009988f80a0384a508d3b85901
uwakwevivian <uwakwevivian@gmail.com>
2023-02-01 06:43:18 -0800

    task 0 update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -21,8 +21,8 @@ else
 			 printf("[%u] %s\n", h->len, h->str);
 
 		 h = h->next;
-		 nodes++;
+		 s++;
 }
 
-return (nodes);
+return (s);
 }
