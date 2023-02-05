2059a45788887868cd6278806091187344341911
uwakwevivian <uwakwevivian@gmail.com>
2023-02-05 13:15:30 -0800

    task 3 update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,33 +11,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	 list_t *new;
-	  list_t *temp = *head;
+	list_t *new;
+	list_t *temp = *head;
 
-	   unsigned int len = 0;
+	unsigned int len = 0;
 
-	    while (str[len])
+	while (str[len])
 
-		     len++;
+	len++;
 
-	     new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 
-	      if (!new)
+	if (!new)
 
-		       return (NULL);
+	return (NULL);
 
-	       new->str = strdup(str);
-	        new->len = len;
-		 new->next = NULL;
-		  if (*head == NULL)
-			   {
-				    *head = new;
-				     return (new);
-				      }
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+	}
+	while (temp->next)
+	temp = temp->next;
 
-		   while (temp->next)
-			    temp = temp->next;
-
-		    temp->next = new;
-		     return (new);
+	temp->next = new;
+	return (new);
 }
