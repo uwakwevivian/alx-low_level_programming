b6b3ff2fc366ca26f547f8949daa4b0c698ca967
uwakwevivian <uwakwevivian@gmail.com>
2023-03-20 10:14:40 -0700

    Dyna lib

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function to copy a string
+ * @dest: Copy destination
+ * @src: Copy source
+ * @n: The number of characters/bytes to be copied
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;/* integer for loop */
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		if (i < n)
+		{
+			*(dest + i) = *(src + i);
+		}
+		else
+			break;
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
